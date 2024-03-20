d8aa46fe1036c47ef0470c1739a3c227221eafe4
Erick Levi <ericklevi564@gmail.com>
2024-03-20 14:43:55 -0300

    Atualizacao do move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,5 +9,50 @@ using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField] private float spd;
+    [SerializeField] private float jumpForce,DoubleForce;
+    private float axisX;
     
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Animator anim;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    //movimentacao do personagem
+    private void Move()
+    {
+        axisX = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(axisX * spd, rb.velocity.y);
+
+        if(axisX > 0)
+        {
+            sr.flipX = false;
+        }
+
+        if(axisX < 0)
+        {
+            sr.flipX = true;
+        }
+
+    }
+
+
+    private void Jump()
+    {
+
+    }
+
 }
