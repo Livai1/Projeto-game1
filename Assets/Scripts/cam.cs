df0e7334ad50bb0fd06f5a0079a165f8e3508050
Erick Levi <ericklevi564@gmail.com>
2024-03-20 12:16:51 -0300

    Arquivos do Jogo

---
+++ Assets/Scripts/cam.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class cam : MonoBehaviour
+{
+    private void Start() 
+    {
+        
+    }
+
+    private void Update() 
+    {
+        
+    }
+}
