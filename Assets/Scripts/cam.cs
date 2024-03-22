52537015d4b84172ef857ad9fffeaa34ba4d1297
Erick Levi <ericklevi564@gmail.com>
2024-03-22 12:28:41 -0300

    cam
    A cam acompanha o jogador e a camera e travada quando a posicao do player e igual a 0

--- Assets/Scripts/cam.cs
+++ Assets/Scripts/cam.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
+using UnityEngine.LowLevel;
 using Vector3 = UnityEngine.Vector3;
 
 public class cam : MonoBehaviour
 {
+    private Transform player;
+    [SerializeField]private float smooth;
+
     private void Start() 
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    private void Update() 
+    private void LateUpdate() 
     {
-        
+        if(player.position.x > 0)
+        {
+        Vector3 following = new Vector3(player.position.x, player.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, following, smooth * Time.deltaTime);
+        }
     }
 }
