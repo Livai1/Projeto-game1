3987db2659bdb6e3002355e89c5832f702637714
Erick Levi <ericklevi564@gmail.com>
2024-03-28 14:05:45 -0300

    "nada"

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,64 +9,23 @@ using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour
 {   
-    [SerializeField] private float spd;
-    [SerializeField] private float jumpForce, doubleForce;
+    private float jumpForce;
     private bool isJumping;
-    private bool doubleJump;
-    private float axisX;
     private Rigidbody2D rb;
     private Animator anim;
 
-    private void Start()
+    private void Start() 
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
-   
+
     private void Update()
     {
-       Jump();
-    }
-
-    private void FixedUpdate()
-    {
-        Move();
-    }
-
-    private void Move()
-    {
-        axisX = Input.GetAxis("Horizontal");
-
-        rb.velocity = new Vector2(axisX * spd, rb.position.y);
-        
-        if(axisX > 0)
+        if(Input.GetButtonDown("Jump"))
         {
-            transform.eulerAngles = new Vector3(0,0,0);
-        }
-
-        if(axisX < 0)
-        {
-            transform.eulerAngles = new Vector3(0,180,0);
+            rb.AddForce(new Vector2(0,jumpForce), ForceMode2D.Impulse); 
         }
     }
 
-    private void Jump()
-    {
-       if(Input.GetButtonDown("Jump"))
-       {
-        if(!isJumping)
-        {        
-            rb.AddForce(new Vector2(0,jumpForce), ForceMode2D.Impulse);
-            isJumping = true;
-        }
-       }
-    }
-
-    private void OnCollisionEnter2D(Collision2D coll)
-    {
-        if(coll.gameObject.layer == 3)
-        {
-            isJumping = false;
-        }
-    }
 }
